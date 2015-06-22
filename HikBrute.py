334d20d1928e734f1eb83211eeaf5bf5c8afed5b
Ismail <xd4rker@gmail.com>
2015-06-22 15:52:35 +0100

    Update HikBrute.py

--- HikBrute.py
+++ HikBrute.py
@@ -18,7 +18,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import httplib2, sys, os.path, signal, hashlib
+import httplib2, sys, os.path, signal, hashlib, json, re
 
 if len(sys.argv) == 1:
 	print "Error: Usage: ./script.py <WORDLIST_PATH>"
@@ -27,6 +27,26 @@ if len(sys.argv) == 1:
 if not os.path.isfile(sys.argv[1]):
 	print "Error: File doesn't exist"
 	sys.exit()
+
+def GetIP(source):
+	regex="var url='(.+?)';"
+	pattern=re.compile(regex)
+	url = re.findall(pattern,source)
+	ip = re.findall('[0-9]+(?:\.[0-9]+){3}', url[0])
+	
+	return ip[0]
+
+def GetLocation(ip):
+	h = httplib2.Http(timeout=100)
+	link = "http://www.ipinfo.io/"+ip.rstrip()
+	resp = h.request(link, headers={'Accept':'application/json'})
+	content = resp[1]
+	json_decoded = json.loads(content)
+
+	info = [ json_decoded['country'], json_decoded['city'] ]
+	info = map(str, info)
+
+	return info
 
 HEADER = "\033[95m"
 BLUE = "\033[94m"
@@ -38,13 +58,13 @@ hostCount = 0;
 NotRegistered = "75df0ef069c0dd3f0ccde6e4d7eabe87"
 NullRedirect = "eb503a81d7f04f9b4537d0bd67b5c9db"
 
+
 wordlist_path = sys.argv[1]
 f = open(wordlist_path)
 lines = f.readlines()
 v = open("liveHosts.list", "a");
 
-h = httplib2.Http(timeout=10)
-#h.follow_all_redirects = False
+h = httplib2.Http(timeout=100)
 
 print "\n" + HEADER + BLUE + INFO + ENDLINE + " " + "Using file : "+wordlist_path+"\n"
 
@@ -53,13 +73,16 @@ for i in range(len(lines)):
 	
 	try:
 		resp = h.request(link, "GET")
-		#location = resp[0]['content-location']
 		content = resp[1]
 		checksum = hashlib.md5(content).hexdigest()
+
 		if (checksum != NotRegistered) & (checksum != NullRedirect):
-			print HEADER + BLUE + INFO + ENDLINE + " " + link
-			#v.write(link+"\n");
+			Addr = GetIP(content)
+			Location = GetLocation(Addr)
+
+			print HEADER + BLUE + INFO + ENDLINE + " " + link + " \t->\t" + Addr + "\t" + Location[0] + " - " + Location[1]
 			hostCount += 1;
+
 	except:
 		pass
 v.close();
